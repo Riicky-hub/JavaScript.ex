f4ba40064ec405397bd3af7d67cf0dd2ae6824ba
riicky-hub <richardjosephace1960@gmail.com>
2022-02-25 21:00:24 -0300

    reformulando estudos, e criando um projeto de calculadora

---
+++ javascript_exercicio/js/exercicio-02.js
@@ -0,0 +1,5 @@
+let n1 = Number(window.prompt('Coloque um número'));
+let n2 = Number(window.prompt('Coloque outro número'));
+let s = n1 + n2;
+let nome = window.prompt('Qual é o seu nome?');
+alert(`Bem-vindo ao site, ${nome}! A soma de ${n1} + ${n2} é ${s}!`);
