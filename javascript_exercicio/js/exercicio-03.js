0b69064a3f3056f93bbdf3ec42f86a9be28c6e8b
riicky-hub <richardjosephace1960@gmail.com>
2022-02-28 19:25:19 -0300

    Criando uma pequena calculadora

---
+++ javascript_exercicio/js/exercicio-03.js
@@ -0,0 +1,4 @@
+let nome = 'Ricardo';
+let nota = 9;
+let resultado = nota >= 7 ? `Parabéns, ${nome}! Você foi aprovado com a nota ${nota}!` : `Lamento, ${nome}. Você foi reprovado com a nota ${nota}!`;
+console.log(resultado);
---
+++ javascript_exercicio/js/small_test_calc/app.js
@@ -0,0 +1,18 @@
+function somar(){
+    let tn1Soma = document.querySelector('input#tn1Soma').value;
+    let tn2Soma = document.querySelector('input#tn2Soma').value;
+    let n1Soma = Number(tn1Soma);
+    let n2Soma = Number(tn2Soma);
+    r = n1Soma + n2Soma;
+    let rText = document.querySelector('h2.resultSoma');
+    rText.innerHTML = (`A soma dos números ${n1Soma} e ${n2Soma} é igual a: ${r}!`);
+}
+function sub() {
+    let tn1Sub = document.querySelector('input#tn1Sub').value;
+    let tn2Sub = document.querySelector('input#tn2Sub').value;
+    let n1Sub = Number(tn1Sub);
+    let n2Sub = Number(tn2Sub);
+    let r = n1Sub - n2Sub;
+    let rText = document.querySelector('h2.resultSub');
+    rText.innerHTML = (`A subtração dos números ${n1Sub} e ${n2Sub} é igual a: ${r}!`)
+}
