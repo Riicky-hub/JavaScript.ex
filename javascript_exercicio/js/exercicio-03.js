70ae264f791dbb9a9d6d27569aff446da4bd0139
riicky-hub <richardjosephace1960@gmail.com>
2022-03-02 18:17:44 -0300

    criando um verificador de horario

--- javascript_exercicio/js/exercicio-03.js
+++
@@ -1,4 +0,0 @@
-let nome = 'Ricardo';
-let nota = 9;
-let resultado = nota >= 7 ? `Parabéns, ${nome}! Você foi aprovado com a nota ${nota}!` : `Lamento, ${nome}. Você foi reprovado com a nota ${nota}!`;
-console.log(resultado);
