bb9a9e3e00f6498373e1ab9744be95f40e3282ff
riicky-hub <richardjosephace1960@gmail.com>
2022-03-04 18:09:37 -0300

    polindo codigo

--- javascript_exercicio/js/verificador_horario/app.js
+++ javascript_exercicio/js/verificador_horario/app.js
@@ -1,21 +1,20 @@
 var agora = new Date();
 var agoraHora = agora.getHours();
-var body = document.querySelector('body');
 var container = document.querySelector('div.container');
 if (agoraHora >= 6 && agoraHora < 12) {
     container.innerHTML += `<p id="container_p">Bom dia! Agora são ${agoraHora} horas.</p>`
     container.innerHTML += `<img src="./img/morning.jpg" alt="Manhã" id="container_img">`
-    body.style.backgroundColor = 'rgb(255, 255, 101)';
+    document.body.style.backgroundColor = 'rgb(255, 255, 101)';
 } else if (agoraHora >= 12 && agoraHora < 16) {
     container.innerHTML += `<p id="container_p">Boa tarde! Agora são ${agoraHora} horas.</p>`
     container.innerHTML += `<img src="./img/afternoon.jpg" alt="Tarde" id="container_img">`
-    body.style.backgroundColor = 'rgb(255, 190, 69)';
+    document.body.style.backgroundColor = 'rgb(255, 190, 69)';
 } else if (agoraHora >= 16 && agoraHora < 18) {
     container.innerHTML += `<p id="container_p">Boa tarde! Agora são ${agoraHora} horas.</p>`
     container.innerHTML += `<img src="./img/evening.jpg" alt="Tarde" id="container_img">`
-    body.style.backgroundColor = 'rgb(255, 255, 101)';
+    document.body.style.backgroundColor = 'rgb(255, 255, 101)';
 } else if (agoraHora >= 18 || agoraHora < 6) {
     container.innerHTML += `<p id="container_p">Boa noite! Agora são ${agoraHora} horas.</p>`
     container.innerHTML += `<img src="./img/night.jpg" alt="noite" id="container_img">`
-    body.style.backgroundColor = 'rgb(31, 31, 104)';
+    document.body.style.backgroundColor = 'rgb(31, 31, 104)';
 }
---
+++ javascript_exercicio/outros/app.js
@@ -0,0 +1,4 @@
+let valores = [1, 2, 5, 4, 7, 2, 4]
+for(let pos=0;pos < valores.length; pos++){
+    console.log(`A variavél na posição ${pos} tem o valor de ${valores[pos]}`);
+}
