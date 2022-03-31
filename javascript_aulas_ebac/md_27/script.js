c5fea8f2c02078ca50f7deecd0d805c1f75ace38
riicky-hub <richardjosephace1960@gmail.com>
2022-03-30 23:11:30 -0300

    Estilizando projeto

--- javascript_aulas_ebac/md_27/script.js
+++ javascript_aulas_ebac/md_27/script.js
@@ -1,18 +1,24 @@
 const url = 'https://api.github.com/users';
-const main = document.querySelector('div.main');
+const main = document.querySelector('div.main_response');
 let text = '';
 
 function getUser(user) {
     fetch(`${url}/${user}`)
     .then((response) => response.json())
     .then((data) => {
-        text += `${data.name} possui ${data.public_repos} repositórios públicos no GitHub como ${data.login}<br>`
+        text += `<p class="main_text">${data.name} possui <span>${data.public_repos} repositórios</span> públicos no GitHub como <span>${data.login}</span></p>`
         main.innerHTML = text;
     })
     .catch((error) => console.error('Erro: ', error.message || error))
 }
-const userInput = document.querySelector('input.username')
 
-userInput.addEventListener('focusout', function(event) {
-    getUser(event.target.value)
+const userInput = document.querySelector('input.main_input');
+const btnInput = document.querySelector('button.main_btn');
+
+btnInput.addEventListener('click', function() {
+    getUser(userInput.value)
 })
+
+//userInput.addEventListener('focusout', function(event) {
+//    getUser(event.target.value)
+//})
