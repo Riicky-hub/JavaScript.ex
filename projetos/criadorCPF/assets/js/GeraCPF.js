610df6029454af3616d2adb32cce295db451b488
riicky-hub <richardjosephace1960@gmail.com>
2022-05-20 23:50:06 -0300

    formatando CPF gerado

--- projetos/criadorCPF/assets/js/GeraCPF.js
+++ projetos/criadorCPF/assets/js/GeraCPF.js
@@ -4,12 +4,18 @@ export default class GeraCPF {
     rand(min = 100000000, max = 999999999) {
         return (String(Math.floor(Math.random() * (max - min) + min)));
     }
+    formatado(cpf) {
+        return  cpf.slice(0, 3) + '.' +
+                cpf.slice(3, 6) + '.' +
+                cpf.slice(6, 9) + '-' +
+                cpf.slice(9, 11);
+    }
     gerar() {
         const cpfLimpo = this.rand();
         const digito1 = ValidaCPF.criaDig(cpfLimpo);
         const digito2 = ValidaCPF.criaDig(cpfLimpo + digito1);
 
         const newCPF = cpfLimpo + digito1 + digito2;
-        return newCPF;
+        return this.formatado(newCPF);
     }
 }
